f46fcdcf80da64f2961f20a31fa71742967fca04
Josh Zimmerman <JoshuaZimmerman@quickenloans.com>
2018-10-26 13:28:15 -0400

    This is my first commit for Assessment 3 - Question 1

--- DevBuild.Assessment3_Question1/Program.cs
+++ DevBuild.Assessment3_Question1/Program.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DevBuild.Utilities;
 
 namespace DevBuild.Assessment3_Question1 {
     class Program {
+        public static string[] mainMenuOptions = { "Display list of Persons", "Add Person", "Delete Person" };
+
         static void Main(string[] args) {
+
+
+
+            Console.Write("***********************************************************\n" +
+              "*               Dev.Build(2.0) - My Peeps                 *\n" +
+              "***********************************************************\n\n");
+
+            MenuHandling.PrintMenuOptions(mainMenuOptions, menuMode: true);
+            var userSelection = UserInput.SelectMenuOption(mainMenuOptions.Length);
+
+            switch (mainMenuOptions[userSelection - 1]) {
+                case "Display List of Persons": break;
+                case "Add Person": break;
+                case "Delete Person": break;
+            }
+
+            while (true) {
+                //Display list for the first time in the loop
+                //Prompt the user to determine whether or not they want to display list, add Person, or delete Person
+
+                
+            }
         }
+
+        public void AddPerson() { }
+
+        public void DeletePerson() { }
     }
 }
